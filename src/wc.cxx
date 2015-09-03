060f0b100541f54fa743f16240a34d3a807d0e79
Boris Egorov <egorov@linux.com>
2015-09-04 00:21:21 +0600

    wc: Implement basic functionality

--- src/wc.cxx
+++ src/wc.cxx
@@ -1,15 +1,27 @@
 #include <wc.h>
 
+#include <sstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-void print_counts(istream&, ostream& out)
+void print_counts(istream& in, ostream& out)
 {
     size_t nlines{0};
     size_t nwords{0};
     size_t nbytes{0};
+
+    for (string s; getline(in, s);) {
+        ++nlines;
+
+        istringstream iss(s);
+        for (string word; iss >> word;)
+            ++nwords;
+
+        nbytes += s.size() + 1; // all characters + newline
+    }
 
     out << nlines << " " << nwords << " " << nbytes;
 }
--- tests/test_wc.cxx
+++ tests/test_wc.cxx
@@ -21,3 +21,37 @@ TEST(WcTest, EmptyPrintsZeros) {
     EXPECT_EQ(0, nwords);
     EXPECT_EQ(0, nbytes);
 }
+
+TEST(WcTest, OnelineOk) {
+    istringstream iss{"a\n"};
+    ostringstream oss;
+
+    print_counts(iss, oss);
+
+    istringstream actual(oss.str());
+    size_t nlines;
+    size_t nwords;
+    size_t nbytes;
+    actual >> nlines >> nwords >> nbytes;
+
+    EXPECT_EQ(1, nlines);
+    EXPECT_EQ(1, nwords);
+    EXPECT_EQ(2, nbytes);
+}
+
+TEST(WcTest, MulipleLinesOk) {
+    istringstream iss{"cat\n  dog\n"};
+    ostringstream oss;
+
+    print_counts(iss, oss);
+
+    istringstream actual(oss.str());
+    size_t nlines;
+    size_t nwords;
+    size_t nbytes;
+    actual >> nlines >> nwords >> nbytes;
+
+    EXPECT_EQ(2, nlines);
+    EXPECT_EQ(2, nwords);
+    EXPECT_EQ(10, nbytes);
+}
