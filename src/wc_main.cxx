3f4251b7d3ad6808ec8b93b22a838f1533d4341f
Boris Egorov <egorov@linux.com>
2015-09-06 15:47:56 +0600

    wc: Align results similar to how real wc does this

--- src/wc_main.cxx
+++ src/wc_main.cxx
@@ -1,5 +1,10 @@
+#include <algorithm>
+#include <cmath>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <vector>
+#include <tuple>
 #include <wc.h>
 
 using namespace std;
@@ -11,11 +16,33 @@ int main(int argc, char *argv[])
     if (argc == 1) {
         print_counts(cin, cout);
     } else {
+        vector<size_t> nlines, nwords, nbytes;
         for (int i = 1; i < argc; ++i) {
             ifstream ifs{argv[i]};
-            print_counts(ifs, cout);
-            cout << " " << argv[i] << endl;
+            size_t nl, nw, nb;
+            tie(nl, nw, nb) = get_counts(ifs);
+            nlines.push_back(nl);
+            nwords.push_back(nw);
+            nbytes.push_back(nb);
         }
+        auto nl_total{accumulate(nlines.begin(), nlines.end(), 0)};
+        auto nw_total{accumulate(nwords.begin(), nwords.end(), 0)};
+        auto nb_total{accumulate(nbytes.begin(), nbytes.end(), 0)};
+
+        size_t nl_width = 1 + round(0.5 + log10(nl_total));
+        size_t nw_width = 1 + round(0.5 + log10(nw_total));
+        size_t nb_width = 1 + round(0.5 + log10(nb_total));
+
+        for (int i = 1; i < argc; ++i) {
+            cout << setw(nl_width) << nlines[i - 1]
+                 << setw(nw_width) << nwords[i - 1]
+                 << setw(nb_width) << nbytes[i - 1]
+                 << " " << argv[i] << endl;
+        }
+        cout << setw(nl_width) << nl_total
+             << setw(nw_width) << nw_total
+             << setw(nb_width) << nb_total
+             << " total" << endl;
     }
 
     return 0;
