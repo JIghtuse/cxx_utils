9b9cb64cb3d1c05148b1b7dff2d0a91ecbe024e6
Boris Egorov <egorov@linux.com>
2015-09-17 00:39:31 +0600

    lurker: Now it is a grep-like utility

--- src/lurker.cxx
+++ src/lurker.cxx
@@ -1,16 +1,46 @@
 #include <boost/filesystem.hpp>
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
-using namespace boost::filesystem;
+namespace fs = boost::filesystem;
 
-/* investigates file system */
+enum class Extension {
+    CXX
+};
+
+using ExtensionDict = std::map<Extension, string>;
+
+void grep_in_file(const char* filename, const char* pattern)
+{
+    ifstream ifs(filename);
+    for (string s; getline(ifs, s);)
+        if (s.find(pattern) != string::npos)
+            cout << s << endl;
+}
+
+void grep_recursively(ExtensionDict& extensions, Extension ext, const char* pattern)
+{
+    const string search_extension{extensions[ext]};
+    fs::path search_path(".");
+
+    for (auto element : fs::recursive_directory_iterator(search_path)) {
+        fs::path p = element;
+        if (is_regular_file(p) && p.extension() == search_extension)
+            grep_in_file(p.c_str(), pattern);
+    }
+}
+
+/* grep-like tool to search in specific files */
 int main()
 {
-    path search_path(".");
+    ExtensionDict extensions;
+    extensions[Extension::CXX] = ".cxx";
 
-    cout << "directory " << search_path << " contents: \n";
-    // for (auto element: search_path)
-        // cout << "\t" << element << endl;
+    grep_recursively(extensions, Extension::CXX, "string");
+
     return 0;
 }
