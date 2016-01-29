0d09bd37e187574fe3f994ac921b90257ad5c3aa
Boris Egorov <egorov@linux.com>
2016-01-29 10:36:29 +0600

    rand_int: Generate random integers in specified range

---
+++ src/rand_int.cxx
@@ -0,0 +1,72 @@
+#include <experimental/optional>
+#include <iostream>
+#include <random>
+#include <sstream>
+
+using namespace std;
+using std::experimental::optional;
+
+class RandomInt {
+public:
+    RandomInt(int from, int to) :
+        rd{},
+        re{rd()},
+        dist{from, to}
+    {}
+    int operator()()
+    {
+        return dist(re);
+    }
+private:
+    random_device rd;
+    default_random_engine re;
+    uniform_int_distribution<int> dist;
+};
+
+template<typename T>
+optional<T> parse_number(const string& s)
+{
+    T value;
+    istringstream iss{s};
+
+    if (iss >> value)
+        return value;
+    return optional<T>{};
+}
+
+template<typename T>
+T parse_number_with_default(const string& s, T default_value)
+{
+    auto n = parse_number<T>(s);
+    if (n)
+        return *n;
+    return default_value;
+}
+
+int main(int argc, char *argv[])
+{
+    auto usage = [argv]() {
+        cerr << "Usage: " << argv[0]
+             << " <from_number> <to_number> [nnumbers =1]\n";
+    };
+    if (argc < 3 || argc > 4) {
+        usage();
+        return 1;
+    }
+
+    const auto from = parse_number<int>(argv[1]);
+    const auto to = parse_number<int>(argv[2]);
+    if (!from || !to) {
+        usage();
+        return 1;
+    }
+    size_t nnumbers = 1;
+    if (argc == 4)
+        nnumbers = parse_number_with_default<size_t>(argv[3], nnumbers);
+
+    RandomInt ri{*from, *to};
+    for (size_t i = 0; i < nnumbers; ++i)
+        cout << ri() << endl;
+
+    return 0;
+}
